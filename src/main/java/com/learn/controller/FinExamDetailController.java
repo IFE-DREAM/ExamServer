602f4472da78fa3e53152a57cc85e1e654e9a228
IFE-DREAM <1914175209@qq.com>
2023-07-15 16:19:53 +0800

    ExamServer

--- src/main/java/com/learn/controller/FinExamDetailController.java
+++ src/main/java/com/learn/controller/FinExamDetailController.java
@@ -1,5 +1,6 @@
 package com.learn.controller;
 
+import com.learn.dto.FinExamDetailDTO;
 import com.learn.service.FinExamDetailService;
 import com.learn.service.FinExamService;
 import com.learn.utils.ResultModel;
@@ -8,6 +9,7 @@ import com.learn.vo.FinExamVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,9 +20,9 @@ public class FinExamDetailController {
     @Autowired
     private FinExamDetailService finExamDetailService;
 
-    @GetMapping("/finExamDetails/{id}") //进入试卷详情页
-    public ResultModel<List<FinExamDetailVO>> findAll(@PathVariable("id") int id){
-        return finExamDetailService.findAll(id);
+    @PostMapping("/finExamDetails/") //进入试卷详情页
+    public ResultModel<List<FinExamDetailVO>> findAll(FinExamDetailDTO finExamDetailDTO){
+        return finExamDetailService.findAll(finExamDetailDTO);
     }
 
 }
